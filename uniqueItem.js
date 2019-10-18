d162739ca499bb6fb0c7be985d20878a88ef8216
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-18 11:12:23 +0300

    added another solution for string unique question

--- uniqueItem.js
+++ uniqueItem.js
@@ -1,14 +1,43 @@
-//find the unique items in an array
-// function should return only "Simon" and "Dean"
+//create a function where it finds the unique items in an array and return them 
 
+// Array: function should return only "Simon" and "Dean"
 const names = ["Hari", "Simon", "Dean", "Koray", "Hari", "Koray"]
 
+// String: function should return true for unique_x and false for notUnique_y once it is passed as a parameter.
+let unique_x = "abcde"
+let notUnique_y = "gbcdabcdefb"
 
+
+
+//String version
+function isItAUniqueString(str) {
+    let values = []
+    // console.log(values)
+
+    for (let letter of str) {
+        // console.log(letter)
+
+        if (values.indexOf(letter) !== -1) {
+            return false
+        }
+
+        values.push(letter)
+    }
+
+    return true
+}
+
+console.log(isItAUniqueString(notUnique_y)) // false
+console.log(isItAUniqueString(unique_x)) // true
+
+
+
+//Array version
 function findUniqueItems(arr) {
     let newUniqueArray = []
 
-    names.forEach((name) => {
-        if (names.indexOf(name) === names.lastIndexOf(name)) {
+    arr.forEach((name) => {
+        if (arr.indexOf(name) === arr.lastIndexOf(name)) {
             newUniqueArray.push(name)
         }
     })
@@ -17,3 +46,4 @@ function findUniqueItems(arr) {
 }
 
 console.log(findUniqueItems(names))
+// Object version
